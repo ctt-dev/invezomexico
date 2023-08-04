4409cd8335e886721d0b9a5758d5178d6e1fee9f
Gilberto <gil.espinosa@coreteam.mx>
2023-08-04 07:36:12 +0000

    [UPDATE] Parametros para llantas

--- ctt_mercado_libre/models/product.py
+++ ctt_mercado_libre/models/product.py
@@ -42,22 +42,41 @@ class CTTMLProductTmplate(models.Model):
     )
     mercadolibre_warranty = fields.Char(string='Garantía', size=256, help='Garantía del producto. Es obligatorio y debe ser un número seguido por una unidad temporal. Ej. 2 meses, 3 años.')
 
+    #ATRIBUTOS OBLIGATORIOS
+    mercadolibre_BRAND = fields.Char(string="Marca")
+    mercadolibre_MODEL = fields.Char(string="Modelo")
+    mercadolibre_LOAD_INDEX = fields.Char(string="Índice de carga")
+    mercadolibre_TIRES_NUMBER = fields.Char(string="Cantidad de llantas")
+    mercadolibre_AUTOMOTIVE_TIRE_ASPECT_RATIO = fields.Char(string="Relación de aspecto")
+    mercadolibre_SECTION_WIDTH = fields.Char(string="Ancho de sección")
+    mercadolibre_RIM_DIAMETER = fields.Char(string="Diámetro del rin")
+
+    #ATRIBUTOS DE INVENXO
+    mercadolibre_LOAD_RANGE = fields.Char(string="Rango de carga")
+    mercadolibre_TERRAIN_TYPE = fields.Char(string="Tipo de terreno")
+    mercadolibre_TIRE_CONSTRUCTION_TYPE = fields.Char(string="Tipo de construcción")
+    mercadolibre_UNIT_TYPE = fields.Char(string="Tipo de unidad")
+    mercadolibre_IS_RUN_FLAT = fields.Char(string="Es run flat")
+
+    def _import_ml_catgory(self):
+        pass
+    
     def predict_category(self):
         self.ensure_one()
 
         params = self.env['ir.config_parameter'].sudo()
         site_id = params.get_param('ctt_mercado_libre.mercado_libre_site_id')
         access_token = params.get_param('ctt_mercado_libre.mercado_libre_token')
-        _logger.warning(site_id)
-        _logger.warning(self.name)
+        # _logger.warning(site_id)
+        # _logger.warning(self.name)
         
         url = "/sites/"+site_id+"/domain_discovery/search?q="+self.name
-        _logger.warning(url)
+        # _logger.warning(url)
 
         api_conector = MeliApi({'access_token': access_token})
         response = api_conector.get(path=url)
-        _logger.warning(response)
-        _logger.warning(response.json())
+        # _logger.warning(response)
+        # _logger.warning(response.json())
 
         data = response.json()
 

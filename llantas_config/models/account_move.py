7fc708a1b53e0cecfeb20be95798319d164f96a2
mega <mega@coreteam.mx>
2024-03-15 21:03:32 +0000

    [ADD] Datos de pronto pago para compras agregado para CONTACTOS, COMPRAS y FACTURAS DE PROVEEDOR.

--- llantas_config/models/account_move.py
+++ llantas_config/models/account_move.py
@@ -14,3 +14,80 @@ class account_move_line_inherit(models.Model):
         string="No. Pedimento",
         tracking=True,
     )
+
+class account_move_inherit(models.Model):
+    _inherit = 'account.move'
+    _description='Account move line'
+    
+    @api.onchange('partner_id')
+    def update_pronto_pago_data(self):
+        self.pronto_pago_dias_vencimiento = self.partner_id.pronto_pago_dias_vencimiento
+        self.pronto_pago_porcentaje = self.partner_id.pronto_pago_porcentaje
+
+    pronto_pago_dias_vencimiento = fields.Integer(
+        string="Pronto pago - Días para vencimiento"
+    )
+
+    pronto_pago_porcentaje = fields.Float(
+        string="Pronto pago - Porcentaje de descuento"
+    )
+    
+    @api.constrains('pronto_pago_dias_vencimiento')
+    def _check_pronto_pago_dias_vencimiento(self):
+        for rec in self:
+            if rec.pronto_pago_dias_vencimiento < 0:
+                raise ValidationError("No puede seleccionar una cantidad negativa para los días de vencimiento de pronto pago. Corrija para continuar...")
+    
+    @api.constrains('pronto_pago_porcentaje')
+    def _check_pronto_pago_porcentaje(self):
+        for rec in self:
+            if rec.pronto_pago_porcentaje < 0 or rec.pronto_pago_porcentaje > 100:
+                raise ValidationError("Solo puede registrar un descuento de entre el 0% y 100% para pronto pago. Corrija para continuar...")
+
+    @api.depends('invoice_date','pronto_pago_dias_vencimiento')
+    def compute_pronto_pago_fecha_vencimiento(self):
+        for rec in self:
+            if rec.invoice_date:
+                rec.pronto_pago_fecha_vencimiento = rec.invoice_date + datetime.timedelta(days=rec.pronto_pago_dias_vencimiento)
+            else:
+                rec.pronto_pago_fecha_vencimiento = False
+    pronto_pago_fecha_vencimiento = fields.Date(
+        string="Pronto pago - Fecha de vencimiento",
+        compute=compute_pronto_pago_fecha_vencimiento,
+        store=True
+    )
+
+    @api.depends('pronto_pago_fecha_vencimiento')
+    def compute_pronto_pago_dias_para_vencimiento(self):
+        for rec in self:
+            if rec.pronto_pago_fecha_vencimiento:
+                # raise UserError(int(rec.pronto_pago_fecha_vencimiento - datetime.date.today()))
+                rec.pronto_pago_dias_para_vencimiento = int((rec.pronto_pago_fecha_vencimiento - datetime.date.today()).days)
+            else:
+                rec.pronto_pago_dias_para_vencimiento = 0
+            # rec.pronto_pago_dias_para_vencimiento = 0
+    pronto_pago_dias_para_vencimiento = fields.Integer(
+        string="Pronto pago - Días para vencimiento",
+        compute=compute_pronto_pago_dias_para_vencimiento,
+        store=False
+    )
+
+    @api.depends('pronto_pago_porcentaje','amount_total')
+    def compute_pronto_pago_descuento(self):
+        for rec in self:
+            rec.pronto_pago_descuento = rec.amount_total * (rec.pronto_pago_porcentaje / 100)
+    pronto_pago_descuento = fields.Float(
+        string="Pronto pago - Descuento",
+        compute=compute_pronto_pago_descuento,
+        store=True
+    )
+
+    @api.depends('pronto_pago_descuento','amount_total')
+    def compute_pronto_pago_total_con_descuento(self):
+        for rec in self:
+            rec.pronto_pago_total_con_descuento = rec.amount_total - rec.pronto_pago_descuento
+    pronto_pago_total_con_descuento = fields.Float(
+        string="Pronto pago - Total con descuento",
+        compute=compute_pronto_pago_total_con_descuento,
+        store=True
+    )
--- llantas_config/models/purchase.py
+++ llantas_config/models/purchase.py
@@ -6,7 +6,7 @@ from odoo.exceptions import ValidationError
 _logger = logging.getLogger(__name__)
 import datetime
 
-class sale_order_inherit(models.Model):
+class purchase_order_line_inherit(models.Model):
     _inherit = 'purchase.order.line'
     _description = 'lineas orden de compra'
 
@@ -19,3 +19,39 @@ class sale_order_inherit(models.Model):
         string="Codigo proveedor",
         tracking=True
     )
+
+
+class purchase_order_inherit(models.Model):
+    _inherit = 'purchase.order'
+    _description = 'Orden de compra'
+    
+    def _prepare_invoice(self):
+        invoice_vals = super()._prepare_invoice()
+        invoice_vals['pronto_pago_dias_vencimiento'] = self.pronto_pago_dias_vencimiento
+        invoice_vals['pronto_pago_porcentaje'] = self.pronto_pago_porcentaje
+        return invoice_vals
+
+    @api.onchange('partner_id')
+    def update_pronto_pago_data(self):
+        self.pronto_pago_dias_vencimiento = self.partner_id.pronto_pago_dias_vencimiento
+        self.pronto_pago_porcentaje = self.partner_id.pronto_pago_porcentaje
+
+    pronto_pago_dias_vencimiento = fields.Integer(
+        string="Pronto pago - Días para vencimiento"
+    )
+
+    pronto_pago_porcentaje = fields.Float(
+        string="Pronto pago - Porcentaje de descuento"
+    )
+    
+    @api.constrains('pronto_pago_dias_vencimiento')
+    def _check_pronto_pago_dias_vencimiento(self):
+        for rec in self:
+            if rec.pronto_pago_dias_vencimiento < 0:
+                raise ValidationError("No puede seleccionar una cantidad negativa para los días de vencimiento de pronto pago. Corrija para continuar...")
+    
+    @api.constrains('pronto_pago_porcentaje')
+    def _check_pronto_pago_porcentaje(self):
+        for rec in self:
+            if rec.pronto_pago_porcentaje < 0 or rec.pronto_pago_porcentaje > 100:
+                raise ValidationError("Solo puede registrar un descuento de entre el 0% y 100% para pronto pago. Corrija para continuar...")

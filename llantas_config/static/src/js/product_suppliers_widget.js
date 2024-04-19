b4bea2c11c72490701ce62d37fa593ed74bf5759
Gilberto <gil.espinosa@coreteam.mx>
2024-04-19 23:32:57 +0000

    [ADD] Widget para detalle de proveedores en lista de productos

--- llantas_config/static/src/js/product_suppliers_widget.js
+++ llantas_config/static/src/js/product_suppliers_widget.js
@@ -8,9 +8,30 @@ import {registry} from "@web/core/registry";
 export class ProductSuppliersWidget extends Component {
     setup() {
         super.setup();
+        this.orm = useService('orm');
+
+        this.state = useState({
+            lines: [],
+            visible: false,
+        });
+
+        this.resId = this.props.record.resId;
+
+        onWillStart(() => this.searchSupplierInfo(this.resId));
 
         console.log(this);
+        // console.log(this.state.visible);
         console.log(this.props.value);
+    }
+
+    async searchSupplierInfo(resId) {
+        const results = await this.orm.searchRead(
+            'product.supplierinfo', 
+            [["product_tmpl_id","=",resId]], 
+            ["partner_id","existencia_actual","ultima_actualizacion","price","currency_id"]
+        );
+        this.state.lines = results;
+        this.state.visible = results.length > 0;
     }
 }
 

63e17bd59f4d19c8d14b858146b500bcc5a3732a
mega <mega@coreteam.mx>
2026-05-20 03:21:52 +0000

    [ADD] Versiones para Odoo19

--- l10n_mx_efos_manager/__manifest__.py
+++ l10n_mx_efos_manager/__manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localization',
-    'version': '1.0',
+    'version': '19.0.1',
 
     # any module necessary for this one to work correctly
     'depends': [
